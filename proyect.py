f5e97862c6f2a295b42358965510ce4864325b8a
Itadoriii <scastroc@utem.cl>
2023-09-22 00:07:32 -0300

    Add
    Se añadio el separador para leer bien los datos, se agrego info de las columnas comentadas

--- proyect.py
+++ proyect.py
@@ -2,9 +2,35 @@
 #INTEGRANTES SEBASTIAN CASTRO, CARLOS PARADA, PABLO ZUÑIGA, DIEGO ADROVEZ.
 import pandas as pd 
 
-bankdata = pd.read_csv("bank-full.csv")
+bankdata = pd.read_csv("bank-full.csv", sep=";")
 #print(bankdata.to_string())  #print de todo el dataframe 
-print(bankdata.shape) #muestra las filas y las columnas existentes en el conjunto de datos, en este caso arroja (45211, 1), lo que es un error
-#print(bankdata)
+print(bankdata.shape) 
+"""
 #bankdata.info()
+
+(45211, 17)
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 45211 entries, 0 to 45210
+Data columns (total 17 columns):
+ #   Column     Non-Null Count  Dtype
+---  ------     --------------  -----
+ 0   age        45211 non-null  int64
+ 1   job        45211 non-null  object
+ 2   marital    45211 non-null  object
+ 3   education  45211 non-null  object
+ 4   default    45211 non-null  object
+ 5   balance    45211 non-null  int64
+ 6   housing    45211 non-null  object
+ 7   loan       45211 non-null  object
+ 8   contact    45211 non-null  object
+ 9   day        45211 non-null  int64
+ 10  month      45211 non-null  object
+ 11  duration   45211 non-null  int64
+ 12  campaign   45211 non-null  int64
+ 13  pdays      45211 non-null  int64
+ 14  previous   45211 non-null  int64
+ 15  poutcome   45211 non-null  object
+ 16  y          45211 non-null  object
+dtypes: int64(7), object(10)
+"""
 
